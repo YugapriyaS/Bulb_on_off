288ea7d573aced4f7a9ea87a60e3d145f37c501b
YugapriyaS <139109080+YugapriyaS@users.noreply.github.com>
2023-07-13 07:23:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var on_btn = document.getElementById("on");
+var off_btn = document.getElementById("off");
+var img = document.getElementById("bulb");
+
+function bulb_on() {
+    img.src="./images/bulb_on.jpg";
+}
+
+function bulb_off() {
+    img.src="./images/bulb_off.jpg";
+}
